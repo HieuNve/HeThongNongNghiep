f58a427a88e76e90e57accbf21798c8b0af5ffc1
nguyenhieu21102001 <59793171+nguyenhieu21102001@users.noreply.github.com>
2021-11-06 02:20:53 +0700

    test1

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import CategoryListItem from "./components/CategoryListItem";
 
+
+const list = [
+    {id: 1, name: "Vườn 1"},
+    {id: 2, name: "Vườn 2"},
+    {id: 3, name: "Vườn 3"},
+    {id: 4, name: "Vườn 4"},
+]
 export default function App() {
+    const categories = list
     return (
-        <View style={styles.container}>
-            <CategoryListItem name={"hágdhasgd"}/>
-            <CategoryListItem name={"Hiếu"}/>
-            <CategoryListItem/>
-            <CategoryListItem/>
-            <CategoryListItem/>
-            <StatusBar style="auto"/>
+        <View>
+            <ScrollView style={{paddingLeft: 16, paddingRight: 16}}>
+                {categories.map(value => <CategoryListItem name={value.name}/>)}
+            </ScrollView>
         </View>
     );
 }
