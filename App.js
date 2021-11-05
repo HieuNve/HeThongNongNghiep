83e5df2ae1d160e645ddd13f8c60802e01e9dede
nguyenhieu21102001 <59793171+nguyenhieu21102001@users.noreply.github.com>
2021-11-06 02:30:58 +0700

    test1

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View, ScrollView} from 'react-native';
+import {StyleSheet, Text, View, ScrollView, FlatList} from 'react-native';
 import CategoryListItem from "./components/CategoryListItem";
 
 
@@ -9,14 +9,20 @@ const list = [
     {id: 2, name: "Vườn 2"},
     {id: 3, name: "Vườn 3"},
     {id: 4, name: "Vườn 4"},
+    {id: 5, name: "Vườn 1"},
+    {id: 6, name: "Vườn 2"},
+    {id: 7, name: "Vườn 3"},
+    {id: 8, name: "Vườn 4"},
 ]
 export default function App() {
     const categories = list
     return (
         <View>
             <ScrollView style={{paddingLeft: 16, paddingRight: 16}}>
-                {categories.map(value => <CategoryListItem name={value.name}/>)}
+                {categories.map(value =>
+                    <CategoryListItem key={value.id} name={value.name}/>)}
             </ScrollView>
+
         </View>
     );
 }
